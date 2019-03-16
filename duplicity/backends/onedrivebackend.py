08571c427b7cb5cda4b6d159b74115bb022dcd15
kenneth@loafman.com <>
2019-03-16 18:23:40 -0500

    * Fix bug #1811114 with revised onedrivebackend.py from David Martin   - Adapt to new Microsoft Graph API

--- duplicity/backends/onedrivebackend.py
+++ duplicity/backends/onedrivebackend.py
@@ -4,6 +4,7 @@
 # Copyright 2014 Google Inc.
 # Contact Michael Stapelberg <stapelberg+duplicity@google.com>
 # This is NOT a Google product.
+# Revised for Microsoft Graph API 2019 by David Martin
 #
 # This file is part of duplicity.
 #
@@ -35,9 +36,11 @@ from duplicity import globals
 from duplicity import log
 
 # For documentation on the API, see
-# http://msdn.microsoft.com/en-us/library/dn659752.aspx
-# http://msdn.microsoft.com/en-us/library/dn631844.aspx
-# https://gist.github.com/rgregg/37ba8929768a62131e85
+# The previous Live SDK API required the use of opaque folder IDs to navigate paths, but the Microsoft Graph
+# API allows the use of parent/child/grandchild pathnames.
+# Old Live SDK API: https://docs.microsoft.com/en-us/previous-versions/office/developer/onedrive-live-sdk/dn659731(v%3doffice.15)
+# Files API: https://docs.microsoft.com/en-us/graph/api/resources/onedrive?view=graph-rest-1.0
+# Large file upload API: https://docs.microsoft.com/en-us/onedrive/developer/rest-api/api/driveitem_createuploadsession?view=odsp-graph-online
 
 
 class OneDriveBackend(duplicity.backend.Backend):
@@ -46,19 +49,26 @@ class OneDriveBackend(duplicity.backend.Backend):
     OAUTH_TOKEN_PATH = os.path.expanduser(
         u'~/.duplicity_onedrive_oauthtoken.json')
 
-    API_URI = u'https://apis.live.net/v5.0/'
-    MAXIMUM_FRAGMENT_SIZE = 60 * 1024 * 1024
-    BITS_1_5_UPLOAD_PROTOCOL = u'{7df0354d-249b-430f-820d-3d2a9bef4931}'
+    API_URI = u'https://graph.microsoft.com/v1.0/'
+    # The large file upload API says that uploaded chunks (except the last) must be multiples of 327680 bytes.
+    REQUIRED_FRAGMENT_SIZE_MULTIPLE = 327680
     CLIENT_ID = u'000000004C12E85D'
-    CLIENT_SECRET = u'k1oR0CbtbvTG9nK1PEDeVW2dzvAaiN4d'
     OAUTH_TOKEN_URI = u'https://login.live.com/oauth20_token.srf'
     OAUTH_AUTHORIZE_URI = u'https://login.live.com/oauth20_authorize.srf'
     OAUTH_REDIRECT_URI = u'https://login.live.com/oauth20_desktop.srf'
-    # wl.skydrive is for reading files,
-    # wl.skydrive_update is for creating/writing files,
-    # wl.offline_access is necessary for duplicity to access onedrive without
+    # Files.Read is for reading files,
+    # Files.ReadWrite  is for creating/writing files,
+    # User.Read is needed for the /me request to see if the token works.
+    # offline_access is necessary for duplicity to access onedrive without
     # the user being logged in right now.
-    OAUTH_SCOPE = [u'wl.skydrive', u'wl.skydrive_update', u'wl.offline_access']
+    OAUTH_SCOPE = [u'Files.Read', u'Files.ReadWrite', u'User.Read', u'offline_access' ]
+
+    # OAUTHLIB_RELAX_TOKEN_SCOPE prevents the oauthlib from complaining about a mismatch between
+    # the requested scope and the delivered scope. We need this because we don't get a refresh
+    # token without asking for offline_access, but Microsoft Graph doesn't include offline_access
+    # in its response (even though it does send a refresh_token).
+
+    os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = 'TRUE'
 
     def __init__(self, parsed_url):
         duplicity.backend.Backend.__init__(self, parsed_url)
@@ -79,19 +89,19 @@ class OneDriveBackend(duplicity.backend.Backend):
                 u'python-requests-oauthlib to be installed. Please install '
                 u'them and try again.\n' + str(e)))
 
-        self.names_to_ids = None
-        self.user_id = None
         self.directory = parsed_url.path.lstrip(u'/')
+        self.directory_onedrive_path = 'me/drive/root:/%s/' % self.directory
         if self.directory == u"":
             raise BackendException((
                 u'You did not specify a path. '
                 u'Please specify a path, e.g. onedrive://duplicity_backups'))
+
         if globals.volsize > (10 * 1024 * 1024 * 1024):
             raise BackendException((
                 u'Your --volsize is bigger than 10 GiB, which is the maximum '
                 u'file size on OneDrive.'))
+
         self.initialize_oauth2_session()
-        self.resolve_directory()
 
     def initialize_oauth2_session(self):
         def token_updater(token):
@@ -120,7 +130,6 @@ class OneDriveBackend(duplicity.backend.Backend):
             token=token,
             auto_refresh_kwargs={
                 u'client_id': self.CLIENT_ID,
-                u'client_secret': self.CLIENT_SECRET,
             },
             auto_refresh_url=self.OAUTH_TOKEN_URI,
             token_updater=token_updater)
@@ -156,7 +165,6 @@ class OneDriveBackend(duplicity.backend.Backend):
 
             token = self.http_client.fetch_token(
                 self.OAUTH_TOKEN_URI,
-                client_secret=self.CLIENT_SECRET,
                 authorization_response=redirected_to)
 
             user_info_response = self.http_client.get(self.API_URI + u'me')
@@ -172,80 +180,32 @@ class OneDriveBackend(duplicity.backend.Backend):
                            u'Original error: %s' % (
                                self.OAUTH_TOKEN_PATH, e)))
 
-        if u'id' not in user_info_response.json():
-            log.Error(u'user info response lacks the "id" field.')
-
-        self.user_id = user_info_response.json()[u'id']
-
-    def resolve_directory(self):
-        u"""Ensures self.directory_id contains the folder id for the path.
-
-        There is no API call to resolve a logical path (e.g.
-        /backups/duplicity/notebook/), so we recursively list directories
-        until we get the object id of the configured directory, creating
-        directories as necessary.
-        """
-        object_id = u'me/skydrive'
-        for component in [x for x in self.directory.split(u'/') if x]:
-            tried_mkdir = False
-            while True:
-                files = self.get_files(object_id)
-                names_to_ids = {x[u'name']: x[u'id'] for x in files}
-                if component not in names_to_ids:
-                    if not tried_mkdir:
-                        self.mkdir(object_id, component)
-                        tried_mkdir = True
-                        continue
-                    raise BackendException((
-                        u'Could not resolve/create directory "%s" on '
-                        u'OneDrive: %s not in %s (files of folder %s)' % (
-                            self.directory, component,
-                            list(names_to_ids.keys()), object_id)))
-                break
-            object_id = names_to_ids[component]
-        self.directory_id = object_id
-        log.Debug(u'OneDrive id for the configured directory "%s" is "%s"' % (
-            self.directory, self.directory_id))
-
-    def mkdir(self, object_id, folder_name):
-        data = {u'name': folder_name, u'description': u'Created by duplicity'}
-        headers = {u'Content-Type': u'application/json'}
-        response = self.http_client.post(
-            self.API_URI + object_id,
-            data=json.dumps(data),
-            headers=headers)
-        response.raise_for_status()
-
-    def get_files(self, path):
-        response = self.http_client.get(self.API_URI + path + u'/files')
-        response.raise_for_status()
-        if u'data' not in response.json():
-            raise BackendException((
-                u'Malformed JSON: expected "data" member in %s' % (
-                    response.json())))
-        return response.json()[u'data']
-
     def _list(self):
-        files = self.get_files(self.directory_id)
-        self.names_to_ids = {x[u'name']: x[u'id'] for x in files}
-        return [x[u'name'] for x in files]
+        accum = []
+        next_url = self.API_URI + self.directory_onedrive_path + u':/children'
+        while True:
+            response = self.http_client.get(next_url)
+            if response.status_code == 404:
+                # No further files here
+                break
+            response.raise_for_status()
+            responseJson = response.json()
+            if u'value' not in responseJson:
+                raise BackendException((
+                    u'Malformed JSON: expected "value" member in %s' % (
+                        responseJson)))
+            accum += responseJson[u'value']
+            if '@odata.nextLink' in responseJson:
+                next_url = responseJson[u'@odata.nextLink']
+            else:
+                break
 
-    def get_file_id(self, remote_filename):
-        u"""Returns the file id from cache, updating the cache if necessary."""
-        if (self.names_to_ids is None or
-                remote_filename not in self.names_to_ids):
-            self._list()
-        return self.names_to_ids.get(remote_filename)
+        return [x[u'name'] for x in accum]
 
     def _get(self, remote_filename, local_path):
         with local_path.open(u'wb') as f:
-            file_id = self.get_file_id(remote_filename)
-            if file_id is None:
-                raise BackendException((
-                    u'File "%s" cannot be downloaded: it does not exist' % (
-                        remote_filename)))
             response = self.http_client.get(
-                self.API_URI + file_id + u'/content', stream=True)
+                self.API_URI + self.directory_onedrive_path + remote_filename + u':/content', stream=True)
             response.raise_for_status()
             for chunk in response.iter_content(chunk_size=4096):
                 if chunk:
@@ -253,92 +213,73 @@ class OneDriveBackend(duplicity.backend.Backend):
             f.flush()
 
     def _put(self, source_path, remote_filename):
+        # Happily, the OneDrive API will lazily create the folder hierarchy required to contain a pathname
+
         # Check if the user has enough space available on OneDrive before even
         # attempting to upload the file.
         source_size = os.path.getsize(source_path.name)
         start = time.time()
-        response = self.http_client.get(self.API_URI + u'me/skydrive/quota')
+        response = self.http_client.get(self.API_URI + u'me/drive?$select=quota')
         response.raise_for_status()
-        if (u'available' in response.json() and
-                source_size > response.json()[u'available']):
-            raise BackendException((
-                u'Out of space: trying to store "%s" (%d bytes), but only '
-                u'%d bytes available on OneDrive.' % (
-                    source_path.name, source_size,
-                    response.json()[u'available'])))
+        if (u'quota' in response.json()):
+            available = response.json()[u'quota'].get(u'remaining', None)
+            if available:
+                log.Debug(u'Bytes available: %d' % available)
+                if source_size > available:
+                    raise BackendException((
+                            u'Out of space: trying to store "%s" (%d bytes), but only '
+                            u'%d bytes available on OneDrive.' % (
+                                source_path.name, source_size,
+                                available)))
         log.Debug(u"Checked quota in %fs" % (time.time() - start))
 
         with source_path.open() as source_file:
             start = time.time()
-            # Create a BITS session, so that we can upload large files.
-            short_directory_id = self.directory_id.split(u'.')[-1]
-            url = u'https://cid-%s.users.storage.live.com/items/%s/%s' % (
-                self.user_id, short_directory_id, remote_filename)
-            headers = {
-                u'X-Http-Method-Override': u'BITS_POST',
-                u'BITS-Packet-Type': u'Create-Session',
-                u'BITS-Supported-Protocols': self.BITS_1_5_UPLOAD_PROTOCOL,
-            }
+            url = self.API_URI + self.directory_onedrive_path + remote_filename + ':/createUploadSession'
 
-            response = self.http_client.post(
-                url,
-                headers=headers)
+            response = self.http_client.post(url)
             response.raise_for_status()
-            if (u'bits-packet-type' not in response.headers or
-                    response.headers[u'bits-packet-type'].lower() != u'ack'):
+            response_json = json.loads(response.content)
+            if u'uploadUrl' not in response_json:
                 raise BackendException((
-                    u'File "%s" cannot be uploaded: '
-                    u'Could not create BITS session: '
-                    u'Server response did not include BITS-Packet-Type: ACK' % (
-                        remote_filename)))
-            bits_session_id = response.headers[u'bits-session-id']
-            log.Debug(u'BITS session id is "%s"' % bits_session_id)
+                        u'File "%s" cannot be uploaded: could not create upload session: %s' % (
+                    remote_filename, response.content)))
+            uploadUrl = response_json[u'uploadUrl']
 
-            # Send fragments (with a maximum size of 60 MB each).
+            # https://docs.microsoft.com/en-us/onedrive/developer/rest-api/api/driveitem_createuploadsession?
+            # indicates 10 MiB is optimal for stable high speed connections.
             offset = 0
+            desired_num_fragments = 10 * 1024 * 1024 // self.REQUIRED_FRAGMENT_SIZE_MULTIPLE
             while True:
-                chunk = source_file.read(self.MAXIMUM_FRAGMENT_SIZE)
+                chunk = source_file.read(desired_num_fragments * self.REQUIRED_FRAGMENT_SIZE_MULTIPLE)
                 if len(chunk) == 0:
                     break
                 headers = {
-                    u'X-Http-Method-Override': u'BITS_POST',
-                    u'BITS-Packet-Type': u'Fragment',
-                    u'BITS-Session-Id': bits_session_id,
+                    u'Content-Length': unicode(len(chunk)),
                     u'Content-Range': u'bytes %d-%d/%d' % (offset, offset + len(chunk) - 1, source_size),
                 }
-                response = self.http_client.post(
-                    url,
+                log.Debug(u'PUT %s %s' % (remote_filename, headers[u'Content-Range']))
+                response = self.http_client.put(
+                    uploadUrl,
                     headers=headers,
                     data=chunk)
                 response.raise_for_status()
                 offset += len(chunk)
 
-            # Close the BITS session to commit the file.
-            headers = {
-                u'X-Http-Method-Override': u'BITS_POST',
-                u'BITS-Packet-Type': u'Close-Session',
-                u'BITS-Session-Id': bits_session_id,
-            }
-            response = self.http_client.post(url, headers=headers)
-            response.raise_for_status()
-
             log.Debug(u"PUT file in %fs" % (time.time() - start))
 
     def _delete(self, remote_filename):
-        file_id = self.get_file_id(remote_filename)
-        if file_id is None:
+        response = self.http_client.delete(self.API_URI + self.directory_onedrive_path + remote_filename)
+        if response.status_code == 404:
             raise BackendException((
                 u'File "%s" cannot be deleted: it does not exist' % (
                     remote_filename)))
-        response = self.http_client.delete(self.API_URI + file_id)
         response.raise_for_status()
 
     def _query(self, remote_filename):
-        file_id = self.get_file_id(remote_filename)
-        if file_id is None:
+        response = self.http_client.get(self.API_URI + self.directory_onedrive_path + remote_filename)
+        if response.status_code != 200:
             return {u'size': -1}
-        response = self.http_client.get(self.API_URI + file_id)
-        response.raise_for_status()
         if u'size' not in response.json():
             raise BackendException((
                 u'Malformed JSON: expected "size" member in %s' % (
