033468a38b7fcc5506e5fd2f20680243b509118d
Aaron A Whitehouse <lists@whitehouse.kiwi.nz>
2019-07-02 23:28:52 +0100

    Convert the docker duplicity_test image to pull the local branch into the container, rather than lp:duplicity. This allows the use of the duplicity Docker testing containers to test local changes in a known-good environment before they are merged into trunk. The equivalent of the old behaviour can be achieved by starting with a clean branch from lp:duplicity. * Expand Docker context to parent branch folder and use -f in the docker build command to point to the Dockerfile. * Simplify build-duplicity_test.sh now that the whole folder is copied (individual files no longer need to be copied)

--- testing/infrastructure/build-duplicity_test.sh
+++ testing/infrastructure/build-duplicity_test.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-cd duplicity_test
-cp -p ../../../requirements.txt .
-cp -p ../id_rsa .
-cp -p ../id_rsa.pub .
-docker build --tag firstprime/duplicity_test:latest .
-rm requirements.txt
-rm id_rsa
-rm id_rsa.pub
-cd ..
+cd ../..
+docker build --tag firstprime/duplicity_test:latest -f testing/infrastructure/duplicity_test/Dockerfile .
 
-cd ftp_server
+cd testing/infrastructure/ftp_server
 docker build --tag firstprime/duplicity_ftp:latest .
 cd ..
 
