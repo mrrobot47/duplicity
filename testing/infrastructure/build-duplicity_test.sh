d9fc47884d9cb202a6952fa26dc063719e0c558b
kenneth@loafman.com <>
2019-07-14 07:46:54 -0500

    * Install python3.6 and 3.7 explicitly in Dockerfile.  Tox and Docker   now support testing Python 2,7, 3.6, and 3.7.

--- testing/infrastructure/build-duplicity_test.sh
+++ testing/infrastructure/build-duplicity_test.sh
@@ -4,18 +4,18 @@ cd duplicity_test
 cp -p ../../../requirements.txt .
 cp -p ../id_rsa .
 cp -p ../id_rsa.pub .
-docker build --tag firstprime/duplicity_test:latest .
+docker build --tag firstprime/duplicity_test .
 rm requirements.txt
 rm id_rsa
 rm id_rsa.pub
 cd ..
 
 cd ftp_server
-docker build --tag firstprime/duplicity_ftp:latest .
+docker build --tag firstprime/duplicity_ftp .
 cd ..
 
 cd ssh_server
 cp -p ../id_rsa.pub .
-docker build --tag firstprime/duplicity_ssh:latest .
+docker build --tag firstprime/duplicity_ssh .
 rm id_rsa.pub
 cd ..
