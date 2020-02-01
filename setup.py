0d1282e687cb121970d07e6b34c2f20cd2192aa9
ken <>
2020-02-01 09:44:10 -0600

    * Reworked setup.py to build a pip-compatible   distribution tarball of duplicity. * Added dist/makepip for convenience.

--- setup.py
+++ setup.py
@@ -32,6 +32,14 @@ from subprocess import Popen, PIPE
 
 from duplicity import __version__
 
+
+# check that we can function here
+if not (sys.version_info[:2] >= (3, 6) or (sys.version_info[0] == 2 and sys.version_info[:2] >= (2, 7))):
+    print(u"Sorry, duplicity requires version 2.7 or version 3.6 or later of Python.")
+    sys.exit(1)
+
+
+# get version string, major.minor.bug.revno
 try:
     bzr = Popen([u"bzr", u"revno"], stdout=PIPE, universal_newlines=True)
     revno = bzr.communicate()[0].split()[0].strip()
@@ -41,10 +49,7 @@ except Exception:
 version_string = __version__ + u'.' + revno
 
 
-if not (sys.version_info[:2] >= (3, 6) or (sys.version_info[0] == 2 and sys.version_info[:2] >= (2, 7))):
-    print(u"Sorry, duplicity requires version 2.7 or version 3.6 or later of Python.")
-    sys.exit(1)
-
+# set incdir and libdir for librsync
 incdir_list = libdir_list = None
 
 if os.name == u'posix':
@@ -58,28 +63,8 @@ if os.name == u'posix':
         incdir_list = [os.path.join(LIBRSYNC_DIR, u'include')]
         libdir_list = [os.path.join(LIBRSYNC_DIR, u'lib')]
 
-data_files = [(u'share/man/man1',
-               [u'bin/duplicity.1',
-                u'bin/rdiffdir.1']),
-              (u'share/doc/duplicity-%s' % version_string,
-               [u'COPYING',
-                u'README',
-                u'README-REPO',
-                u'README-LOG',
-                u'CHANGELOG']),
-              ]
 
-top_dir = os.path.dirname(os.path.abspath(__file__))
-assert os.path.exists(os.path.join(top_dir, u"po")), u"Missing 'po' directory."
-for root, dirs, files in os.walk(os.path.join(top_dir, u"po")):
-    for file in files:
-        path = os.path.join(root, file)
-        if path.endswith(u"duplicity.mo"):
-            lang = os.path.split(root)[-1]
-            data_files.append(
-                (u'share/locale/%s/LC_MESSAGES' % lang,
-                 [u"po/%s/duplicity.mo" % lang]))
-
+# READTHEDOCS uses setup.py sdist but can't handle extensions
 if not os.environ.get(u'READTHEDOCS') == u'True':
     ext_modules=[Extension(name=r"duplicity._librsync",
                            sources=[r"duplicity/_librsyncmodule.c"],
@@ -88,6 +73,58 @@ if not os.environ.get(u'READTHEDOCS') == u'True':
                            libraries=[u"rsync"])]
 else:
     ext_modules = []
+
+
+def get_data_files():
+    '''gen list of data files'''
+
+    # static data files
+    data_files = [
+            (u'share/man/man1',
+                [
+                u'bin/duplicity.1',
+                u'bin/rdiffdir.1'
+                ]
+            ),
+            (u'share/doc/duplicity-%s' % version_string,
+                [
+                u'AUTHORS',
+                u'CHANGELOG',
+                u'Changelog.GNU',
+                u'COPYING',
+                u'README',
+                u'README-LOG',
+                u'README-REPO',
+                u'README-TESTING',
+                ],
+            ),
+        ]
+
+    # msgfmt the translation files
+    assert os.path.exists(u"po"), u"Missing 'po' directory."
+
+    if os.path.exists(u'po/LINGUAS'):
+        linguas = open(u'po/LINGUAS').readlines()
+        for line in linguas:
+            langs = line.split()
+            for lang in langs:
+                try:
+                    os.mkdir(os.path.join(u"po", lang)), lang
+                except os.error:
+                    pass
+                assert not os.system(u"cp po/%s.po po/%s" % (lang, lang)), lang
+                assert not os.system(u"msgfmt po/%s.po -o po/%s/duplicity.mo" % (lang, lang)), lang
+
+    for root, dirs, files in os.walk(u"po"):
+        for file in files:
+            path = os.path.join(root, file)
+            if path.endswith(u"duplicity.mo"):
+                lang = os.path.split(root)[-1]
+                data_files.append(
+                    (u'share/locale/%s/LC_MESSAGES' % lang,
+                     [u"po/%s/duplicity.mo" % lang]))
+
+    return data_files
 
 
 class TestCommand(test):
@@ -102,7 +139,7 @@ class TestCommand(test):
 
         # make symlinks for test data
         if build_cmd.build_lib != top_dir:
-            for path in [u'testfiles.tar.gz', u'gnupg']:
+            for path in [u'source_files.tar.gz', u'gnupg']:
                 src = os.path.join(top_dir, u'testing', path)
                 target = os.path.join(build_cmd.build_lib, u'testing', path)
                 try:
@@ -127,6 +164,7 @@ class InstallCommand(install):
         self.skip_build = True
 
         # This should always be true, but just to make sure!
+        top_dir = os.path.dirname(os.path.abspath(__file__))
         if self.build_lib != top_dir:
             testing_dir = os.path.join(self.build_lib, u'testing')
             os.system(u"rm -rf %s" % testing_dir)
@@ -134,17 +172,8 @@ class InstallCommand(install):
         install.run(self)
 
 
-class SDistCommand(sdist):
-
-    def run(self):
-        version = version_string
-        os.system(os.path.join(top_dir, u"dist", u"makedist"))
-        os.system(u"mkdir -p " + self.dist_dir)
-        os.system(u"mv duplicity-" + version + u".tar.gz " + self.dist_dir)
-
-
-# don't touch my shebang
 class BSCommand (build_scripts):
+    '''Build but don't touch my shebang!'''
 
     def run(self):
         u"""
@@ -183,52 +212,67 @@ class BSCommand (build_scripts):
                                  file, oldmode, newmode)
                         os.chmod(file, newmode)
 
-with open("README") as f:
-    long_description = f.read()
+with open("README") as fh:
+    long_description = fh.read()
 
 setup(name=u"duplicity",
     version=version_string,
     description=u"Encrypted backup using rsync algorithm",
     long_description=long_description,
-    long_description_content_type="text/plain",
+    long_description_content_type=u"text/plain",
     author=u"Ben Escoto <ben@emrose.org>",
     author_email=u"ben@emrose.org",
     maintainer=u"Kenneth Loafman <kenneth@loafman.com>",
     maintainer_email=u"kenneth@loafman.com",
     url=u"http://duplicity.nongnu.org/index.html",
-    packages=[u'duplicity',
-              u'duplicity.backends',
-              u'duplicity.backends.pyrax_identity',
-              u'po',
-              u'testing',
-              u'testing.functional',
-              u'testing.overrides',
-              u'testing.unit'],
-    package_dir={u"duplicity": u"duplicity",
-                 u"duplicity.backends": u"duplicity/backends", },
+    packages=[
+        u'duplicity',
+        u'duplicity.backends',
+        u'duplicity.backends.pyrax_identity',
+        u'testing',
+        u'testing.functional',
+        u'testing.overrides',
+        u'testing.unit',
+        ],
+    package_dir={
+        u"duplicity": u"duplicity",
+        u"duplicity.backends": u"duplicity/backends",
+        },
     ext_modules=ext_modules,
     scripts=[u'bin/rdiffdir', u'bin/duplicity'],
-    data_files=data_files,
-    install_requires=[u'fasteners', u'future'],
-    python_requires='>2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4',
-    tests_require=[u'pytest', u'pytest-runner', u'fasteners', u'future', u'mock', u'pexpect'],
+    data_files=get_data_files(),
+    install_requires=[
+        u'fasteners',
+        u'future'
+        ],
+    tests_require=[
+        u'pytest',
+        u'pytest-runner',
+        u'fasteners',
+        u'future',
+        u'mock',
+        u'pexpect'
+        ],
     test_suite=u'testing',
-    cmdclass={u'test': TestCommand,
-              u'install': InstallCommand,
-              u'sdist': SDistCommand,
-              u'build_scripts': BSCommand},
-    classifiers=[u"Development Status :: 6 - Mature",
-                 u"Environment :: Console",
-                 u"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-                 u"Operating System :: MacOS",
-                 u"Operating System :: POSIX :: Linux",
-                 u"Programming Language :: C",
-                 u"Programming Language :: Python :: 2",
-                 u"Programming Language :: Python :: 2.7",
-                 u"Programming Language :: Python :: 3",
-                 u"Programming Language :: Python :: 3.6",
-                 u"Programming Language :: Python :: 3.7",
-                 u"Programming Language :: Python :: 3.8",
-                 u"Topic :: System :: Archiving :: Backup",
-                 u"Topic :: Utilities"]
+    cmdclass={
+        u'test': TestCommand,
+        u'install': InstallCommand,
+        u'build_scripts': BSCommand
+        },
+    classifiers=[
+        u"Development Status :: 6 - Mature",
+        u"Environment :: Console",
+        u"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        u"Operating System :: MacOS",
+        u"Operating System :: POSIX",
+        u"Programming Language :: Python :: 2",
+        u"Programming Language :: Python :: 2.7",
+        u"Programming Language :: Python :: 3",
+        u"Programming Language :: Python :: 3.6",
+        u"Programming Language :: Python :: 3.7",
+        u"Programming Language :: Python :: 3.8",
+        u"Topic :: System :: Archiving :: Backup"
+        ],
+    python_requires=u'>2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4',
+    platforms=[u'any'],
     )
