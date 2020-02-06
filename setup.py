38800f23229c1fbf15b58109f50d8b31e1b4ae71
ken <>
2020-02-06 11:10:33 -0600

    * add gettext for readthedocs builder

--- setup.py
+++ setup.py
@@ -252,6 +252,7 @@ setup(name=u"duplicity",
     data_files=get_data_files(),
     install_requires=[
         u"fasteners",
+        u"gettext",
         u"intltool",
         u"future"
         ],
