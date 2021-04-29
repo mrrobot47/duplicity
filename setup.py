684dff91cd16bbda2013ae479adaa2478a47702e
Kenneth Loafman <kenneth@loafman.com>
2021-04-29 13:04:41 -0500

    chg:pkg:Change version for LP.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ try:
     from setuptools_scm import get_version  # pylint: disable=import-error
     Version = get_version(**scm_version_args)
 except Exception as e:
-    Version = u"0.8.18.dev"
+    Version = u"0.8.19"
     print(u"Unable to get SCM version: defaulting to %s" % (Version,))
 Reldate = time.strftime(u"%B %d, %Y", time.localtime())
 
