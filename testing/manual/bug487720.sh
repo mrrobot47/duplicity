b840f28f12798e18b1ad84ca0797dffb1281f5bf
Kenneth Loafman <kenneth@loafman.com>
2021-06-23 11:45:04 -0500

    fix:dev:Release lockfile only once.

---
+++ testing/manual/bug487720.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -x -e
+
+for x in 0 1 2 3 4 5 6 7 8 9; do
+    echo "\nLoop $x\n"
+
+    # ----- cleanup -----
+    rm -r /Volumes/home/testdup
+
+    # ----- making initial backup -----
+    PYTHONPATH=. bin/duplicity --no-encr --no-print -v0 --name=foo /usr/bin file:///Volumes/home/testdup --volsize=2 &
+    pid=$!
+    sleep 10
+
+    # ----- kill after 10 seconds ------
+    kill -9 $pid
+    sleep 1
+
+    # ----- restarting first backup -----
+    PYTHONPATH=. bin/duplicity --no-encr --no-print -v0 --name=foo /usr/bin file:///Volumes/home/testdup --volsize=2
+
+    # ----- verifying backup -----
+    PYTHONPATH=. bin/duplicity verify --no-encr --name=foo file:///Volumes/home/testdup /usr/bin --volsize=2
+
+    if [ $# != 0 ] ; then
+    echo "----- Guh!  We hit the bug! -----"
+    exit $#
+    else
+    echo "----- Yay!  We didn't hit the bug! -----"
+    fi
+done
