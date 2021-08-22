327889cbb45da3d5dd15f368b6338a131a624cac
Kenneth Loafman <kenneth@loafman.com>
2021-08-22 12:04:35 -0500

    chg:dev:Some tweaks to run cleaner.

--- testing/manual/issue79.sh
+++ testing/manual/issue79.sh
@@ -11,17 +11,18 @@ alias python3=python3.9
 
 for MODE in mirror stripe; do
 
-    echo "----- Testing ${MODE} mode -----\n"
+    echo -e "\n----- Testing ${MODE} mode -----\n"
 
+    rm -rf ~/.cache/duplicity/issue79
     rm -rf /tmp/first_drive /tmp/second_drive
 
-    bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
-    bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
-    bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
-    bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
-    bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+    bin/duplicity full --name=issue79 ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+    bin/duplicity full --name=issue79 ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+    bin/duplicity full --name=issue79 ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+    bin/duplicity full --name=issue79 ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+    bin/duplicity full --name=issue79 ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
 
-    bin/duplicity remove-all-but-n-full 1 --force multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
-    bin/duplicity collection-status multi:///`pwd`/testing/manual/issue79.json?mode=
+    bin/duplicity remove-all-but-n-full 1 --force --name=issue79 multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+    bin/duplicity collection-status --name=issue79 multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
 
 done
