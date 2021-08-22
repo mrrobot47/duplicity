855ac5fc9e0b54d7c6e1a8c68d66717904213cc0
Kenneth Loafman <kenneth@loafman.com>
2021-08-22 11:53:58 -0500

    fix:dev:Test with mirror and stripe modes.

--- testing/manual/issue79.sh
+++ testing/manual/issue79.sh
@@ -9,10 +9,19 @@ alias python3=python3.9
 
 #export PYDEVD=True
 
-bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=mirror
-bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=mirror
-bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=mirror
-bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=mirror
-bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=mirror
+for MODE in mirror stripe; do
 
-bin/duplicity remove-all-but-n-full 1 --force multi:///`pwd`/testing/manual/issue79.json?mode=mirror
+    echo "----- Testing ${MODE} mode -----\n"
+
+    rm -rf /tmp/first_drive /tmp/second_drive
+
+    bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+    bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+    bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+    bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+    bin/duplicity full ../duplicity-web multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+
+    bin/duplicity remove-all-but-n-full 1 --force multi:///`pwd`/testing/manual/issue79.json?mode=${MODE}
+    bin/duplicity collection-status multi:///`pwd`/testing/manual/issue79.json?mode=
+
+done
