5f2dcd6f0c98dffbe85695caeef001204b463867
Kenneth Loafman <kenneth@loafman.com>
2022-01-11 10:12:04 -0600

    new:test:Add testing/manual/issue98.sh to test issue #98.
    [skip ci]

---
+++ testing/manual/issue98.sh
@@ -0,0 +1,78 @@
+#!/bin/bash -v -e
+
+cd `dirname $0`/../..
+
+export PYTHONPATH=`pwd`
+
+alias python3=python3.10
+
+#export PYDEVD=True
+
+rm -rf ~/.cache/duplicity/issue98 /tmp/first_drive /tmp/second_drive
+
+bin/duplicity \
+    full \
+    --name issue98 \
+    --volsize 5 \
+    --no-encryption \
+    ../duplicity-web-git \
+    file:///tmp/first_drive
+
+bin/duplicity \
+    inc \
+    --name issue98 \
+    --volsize 5 \
+    --no-encryption \
+    ../duplicity-web-git \
+    file:///tmp/first_drive
+
+bin/duplicity \
+    verify \
+    --name issue98 \
+    --volsize 5 \
+    --no-encryption \
+    file:///tmp/first_drive \
+    ../duplicity-web-git
+
+ls -lR ~/.cache/duplicity/issue98 /tmp/first_drive
+
+bin/duplicity \
+    replicate \
+    --name issue98 \
+    --volsize 5 \
+    --no-encryption \
+    --verbosity 9 \
+    file:///tmp/first_drive \
+    file:///tmp/second_drive
+
+bin/duplicity \
+    collection-status \
+    --name issue98 \
+    --volsize 5 \
+    --no-encryption \
+    file:///tmp/first_drive
+
+bin/duplicity \
+    collection-status \
+    --name issue98 \
+    --volsize 5 \
+    --no-encryption \
+    file:///tmp/second_drive
+
+bin/duplicity \
+    verify \
+    --name issue98 \
+    --volsize 5 \
+    --no-encryption \
+    file:///tmp/first_drive \
+    ../duplicity-web-git
+
+bin/duplicity \
+    verify \
+    --name issue98 \
+    --volsize 5 \
+    --no-encryption \
+    file:///tmp/second_drive \
+    ../duplicity-web-git
+
+ls -lR ~/.cache/duplicity/issue98 /tmp/first_drive /tmp/second_drive
